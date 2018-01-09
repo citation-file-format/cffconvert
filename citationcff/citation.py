4de5b54860e07a04865d89d76ca2d124aa173983
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-01-09 15:22:09 +0100

    added cffcitation to bibtex converter

---
+++ citationcff/citation.py
@@ -0,0 +1,77 @@
+import requests
+import yaml
+import re
+
+
+class Citation:
+
+    def __init__(self, url):
+        self.url = url
+        self.baseurl = "https://raw.githubusercontent.com"
+        self.file = None
+        self.file_url = None
+        self.as_yaml = None
+
+    def retrieve_file(self):
+
+        regexp = re.compile(r"^(?P<baseurl>https://github\.com)/(?P<user>[^/\n]*)/(?P<repo>[^/\n]*)(/(?P<branch>[^/\n]*))?",
+            re.IGNORECASE)
+        matched = re.match(regexp, self.url).groupdict()
+
+        self.file_url = "/".join([self.baseurl,
+                                  matched["user"],
+                                  matched["repo"],
+                                  matched["branch"] if matched["branch"] is not None else "master",
+                                           "CITATION"])
+
+        r = requests.get(self.file_url)
+        if r.ok:
+            self.file = r.text
+        else:
+            raise Warning("status not 200 OK")
+
+    def parse_yaml(self):
+        self.as_yaml = yaml.safe_load(self.file)
+
+    def as_bibtex(self):
+
+        def get_author_string():
+            arr = list()
+            for author in self.as_yaml["authors"]:
+                s = list()
+                if "given-names" in author:
+                    s.append(author["given-names"])
+                if "name-particle" in author:
+                    s.append(author["name-particle"])
+                if "family-names" in author:
+                    s.append(author["family-names"])
+                arr.append(" " * 12 + " ".join(s))
+            return " and\n".join(arr) + "\n"
+
+        width = 8
+        s = ""
+        s += "@misc{"
+        s += "your_ref,\n"
+        s += "author".ljust(width, " ") + " = {\n"
+        s += get_author_string()
+        s += " " * 11 + "},\n"
+        s += "title".ljust(width, " ") + " = {"
+        s += self.as_yaml["title"] + "},\n"
+        s += "month".ljust(width, " ") + " = {"
+        s += str(self.as_yaml["date-released"].month) + "},\n"
+        s += "year".ljust(width, " ") + " = {"
+        s += str(self.as_yaml["date-released"].year) + "},\n"
+        s += "doi".ljust(width, " ") + " = {"
+        s += self.as_yaml["doi"] + "},\n"
+        s += "url".ljust(width, " ") + " = {"
+        s += self.as_yaml["repository"] + "}\n"
+        s += "}"
+
+        return s
+
+    def as_csl(self):
+        pass
+
+    def as_enw(self):
+        pass
+
---
+++ test/citation_test.py
@@ -0,0 +1,30 @@
+import unittest
+from citationcff.citation import Citation
+
+
+class CitationTest(unittest.TestCase):
+
+    def setUp(self):
+        url = "https://github.com/jspaaks/trellis"
+        self.citation = Citation(url)
+
+    def test_retrieving_a_url(self):
+        self.assertTrue(self.citation.file is None)
+        self.citation.retrieve_file()
+        self.assertFalse(self.citation.file is None)
+
+    def test_parsing_yaml(self):
+        self.citation.retrieve_file()
+        self.assertTrue(self.citation.as_yaml is None)
+        self.citation.parse_yaml()
+        self.assertFalse(self.citation.as_yaml is None)
+
+    def test_printing_as_bibtex(self):
+        self.citation.retrieve_file()
+        self.citation.parse_yaml()
+        bibtex_str = self.citation.as_bibtex()
+        self.assertEqual("@misc", bibtex_str[:5])
+
+
+if __name__ == "__main__":
+    unittest.main()

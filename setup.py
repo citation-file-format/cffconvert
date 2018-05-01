2c6be0e0351fc3dc2e6602863227d0d6116b01a7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-05-01 10:39:35 +0200

    updated the long description for PyPI/pip install

--- setup.py
+++ setup.py
@@ -36,13 +36,57 @@ setup(
     setup_requires=['pytest', 'pytest-runner'],
     tests_require=['pytest', 'pytest-runner'],
     long_description="""
-Read CFF formatted CITATION file from a GitHub url and convert it to various formats. Currently supported export formats are
-- BibTeX
-- EndNote
-- RIS
-- codemeta
-- plain JSON
-- Zenodo JSON
+Read [CFF formatted CITATION](https://github.com/citation-file-format) file from a GitHub
+url and convert it to various formats, such as:
 
-``cffconvert`` does not support the full CFF spec yet."""
+1. BibTeX
+1. EndNote
+1. RIS
+1. codemeta
+1. plain JSON
+1. Zenodo JSON
+
+``cffconvert`` does not support the full
+[CFF spec](https://citation-file-format.github.io/assets/pdf/cff-specifications-1.0.3.pdf) yet.
+
+
+
+# For users
+
+## Install
+
+```bash
+pip install git+https://github.com/citation-file-format/cff-converter-python.git
+```
+
+# For developers
+
+## Install
+
+```bash
+# get a copy of the cff-converter-python software
+git clone https://github.com/citation-file-format/cff-converter-python.git
+# change directory into cff-converter-python
+cd cff-converter-python
+# make a Python3.5 virtual environment named .venv35
+virtualenv -p /usr/bin/python3.5 .venv35
+# activate the virtual environment
+source ./.venv35/bin/activate
+# install any packages that cff-converter-python needs
+pip install -r requirements.txt
+```
+
+
+## Running tests
+
+```bash
+# (from the project root)
+
+# run unit tests
+pytest test/
+
+# run tests against live system (GitHub)
+pytest livetest/
+```
+"""
 )

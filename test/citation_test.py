84483bdfd49b8077b8faca4d972411c603ed46d3
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-05-01 14:12:36 +0200

    organized the fixtures a bit; documented the validation of some fixtures

--- test/citation_test.py
+++ test/citation_test.py
@@ -9,42 +9,42 @@ class CitationTest1(unittest.TestCase):
     def setUp(self):
         url = "not used in unit testing"
         self.citation = Citation(url, instantiate_empty=True)
-        fixture = os.path.join("fixtures", "citationcff-1")
+        fixture = os.path.join("fixtures", "1", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
         self.citation._parse_yaml()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-1")
+        fixture = os.path.join("fixtures", "1", "bibtex.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-1")
+        fixture = os.path.join("fixtures", "1", "codemeta.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-1")
+        fixture = os.path.join("fixtures", "1", "endnote.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-1")
+        fixture = os.path.join("fixtures", "1", "ris.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
         self.assertEqual(expected_ris, actual_ris)
 
     def test_printing_as_zenodojson(self):
-        fixture = os.path.join("fixtures", "zenodojson-1")
+        fixture = os.path.join("fixtures", "1", "zenodo.json")
         with open(fixture) as f:
             expected_zenodojson = f.read()
         actual_zenodojson = self.citation.as_zenodojson()
@@ -56,42 +56,42 @@ class CitationTest2(unittest.TestCase):
     def setUp(self):
         url = "not used in unit testing"
         self.citation = Citation(url, instantiate_empty=True)
-        fixture = os.path.join("fixtures", "citationcff-2")
+        fixture = os.path.join("fixtures", "2", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
         self.citation._parse_yaml()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-2")
+        fixture = os.path.join("fixtures", "2", "bibtex.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-2")
+        fixture = os.path.join("fixtures", "2", "codemeta.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-2")
+        fixture = os.path.join("fixtures", "2", "endnote.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-2")
+        fixture = os.path.join("fixtures", "2", "ris.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
         self.assertEqual(expected_ris, actual_ris)
 
     def test_printing_as_zenodojson(self):
-        fixture = os.path.join("fixtures", "zenodojson-2")
+        fixture = os.path.join("fixtures", "2", "zenodo.json")
         with open(fixture) as f:
             expected_zenodojson = f.read()
         actual_zenodojson = self.citation.as_zenodojson()
@@ -103,42 +103,42 @@ class CitationTest3(unittest.TestCase):
     def setUp(self):
         url = "not used in unit testing"
         self.citation = Citation(url, instantiate_empty=True)
-        fixture = os.path.join("fixtures", "citationcff-3")
+        fixture = os.path.join("fixtures", "3", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
         self.citation._parse_yaml()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-3")
+        fixture = os.path.join("fixtures", "3", "bibtex.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-3")
+        fixture = os.path.join("fixtures", "3", "codemeta.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-3")
+        fixture = os.path.join("fixtures", "3", "endnote.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-3")
+        fixture = os.path.join("fixtures", "3", "ris.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
         self.assertEqual(expected_ris, actual_ris)
 
     def test_printing_as_zenodojson(self):
-        fixture = os.path.join("fixtures", "zenodojson-3")
+        fixture = os.path.join("fixtures", "3", "zenodo.json")
         with open(fixture) as f:
             expected_zenodojson = f.read()
         actual_zenodojson = self.citation.as_zenodojson()
@@ -150,7 +150,7 @@ class CitationTest4(unittest.TestCase):
     def setUp(self):
         url = "not used in unit testing"
         self.citation = Citation(url, instantiate_empty=True)
-        fixture = os.path.join("fixtures", "citationcff-4")
+        fixture = os.path.join("fixtures", "4", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -172,7 +172,7 @@ class CitationTestOverride(unittest.TestCase):
             "date-released": datetime.datetime.strptime("2018-03-05", "%Y-%m-%d").date()
         }
         self.citation = Citation(url, instantiate_empty=True, override=override)
-        fixture = os.path.join("fixtures", "citationcff-1")
+        fixture = os.path.join("fixtures", "1", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -180,28 +180,28 @@ class CitationTestOverride(unittest.TestCase):
         self.citation._override_suspect_keys()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-5")
+        fixture = os.path.join("fixtures", "5", "bibtex.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-5")
+        fixture = os.path.join("fixtures", "5", "codemeta.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-5")
+        fixture = os.path.join("fixtures", "5", "endnote.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-5")
+        fixture = os.path.join("fixtures", "5", "ris.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
@@ -215,7 +215,7 @@ class CitationTestRemoveSuspectKeyDate(unittest.TestCase):
         url = "not used in unit testing"
         remove = ["date-released"]
         self.citation = Citation(url, instantiate_empty=True, remove=remove)
-        fixture = os.path.join("fixtures", "citationcff-6")
+        fixture = os.path.join("fixtures", "6", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -223,28 +223,28 @@ class CitationTestRemoveSuspectKeyDate(unittest.TestCase):
         self.citation._remove_suspect_keys()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-6-no-date")
+        fixture = os.path.join("fixtures", "6", "bibtex-no-date.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-6-no-date")
+        fixture = os.path.join("fixtures", "6", "codemeta-no-date.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-6-no-date")
+        fixture = os.path.join("fixtures", "6", "endnote-no-date.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-6-no-date")
+        fixture = os.path.join("fixtures", "6", "ris-no-date.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
@@ -258,7 +258,7 @@ class CitationTestRemoveSuspectKeyDoi(unittest.TestCase):
         url = "not used in unit testing"
         remove = ["doi"]
         self.citation = Citation(url, instantiate_empty=True, remove=remove)
-        fixture = os.path.join("fixtures", "citationcff-6")
+        fixture = os.path.join("fixtures", "6", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -266,28 +266,28 @@ class CitationTestRemoveSuspectKeyDoi(unittest.TestCase):
         self.citation._remove_suspect_keys()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-6-no-doi")
+        fixture = os.path.join("fixtures", "6", "bibtex-no-doi.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-6-no-doi")
+        fixture = os.path.join("fixtures", "6", "codemeta-no-doi.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-6-no-doi")
+        fixture = os.path.join("fixtures", "6", "endnote-no-doi.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-6-no-doi")
+        fixture = os.path.join("fixtures", "6", "ris-no-doi.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
@@ -301,7 +301,7 @@ class CitationTestRemoveSuspectKeyVersion(unittest.TestCase):
         url = "not used in unit testing"
         remove = ["version"]
         self.citation = Citation(url, instantiate_empty=True, remove=remove)
-        fixture = os.path.join("fixtures", "citationcff-6")
+        fixture = os.path.join("fixtures", "6", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -309,28 +309,28 @@ class CitationTestRemoveSuspectKeyVersion(unittest.TestCase):
         self.citation._remove_suspect_keys()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-6-no-version")
+        fixture = os.path.join("fixtures", "6", "bibtex-no-version.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-6-no-version")
+        fixture = os.path.join("fixtures", "6", "codemeta-no-version.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-6-no-version")
+        fixture = os.path.join("fixtures", "6", "endnote-no-version.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-6-no-version")
+        fixture = os.path.join("fixtures", "6", "ris-no-version.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()
@@ -344,7 +344,7 @@ class CitationTestRemoveNonExistentKey(unittest.TestCase):
         url = "not used in unit testing"
         remove = ["hjjshbdjsu3933"]
         self.citation = Citation(url, instantiate_empty=True, remove=remove)
-        fixture = os.path.join("fixtures", "citationcff-1")
+        fixture = os.path.join("fixtures", "1", "citation.cff")
         with open(fixture) as f:
             self.citation.file_contents = f.read()
         self.citation.file_url = "not used in unit testing"
@@ -352,28 +352,28 @@ class CitationTestRemoveNonExistentKey(unittest.TestCase):
         self.citation._remove_suspect_keys()
 
     def test_printing_as_bibtex(self):
-        fixture = os.path.join("fixtures", "bibtex-1")
+        fixture = os.path.join("fixtures", "1", "bibtex.txt")
         with open(fixture) as f:
             expected_bibtex = f.read()
         actual_bibtex = self.citation.as_bibtex()
         self.assertEqual(expected_bibtex, actual_bibtex)
 
     def test_printing_as_codemeta(self):
-        fixture = os.path.join("fixtures", "codemeta-1")
+        fixture = os.path.join("fixtures", "1", "codemeta.json")
         with open(fixture) as f:
             expected_codemeta = f.read()
         actual_codemeta = self.citation.as_codemeta()
         self.assertEqual(expected_codemeta, actual_codemeta)
 
     def test_printing_as_enw(self):
-        fixture = os.path.join("fixtures", "endnote-1")
+        fixture = os.path.join("fixtures", "1", "endnote.txt")
         with open(fixture) as f:
             expected_endnote = f.read()
         actual_endnote = self.citation.as_enw()
         self.assertEqual(expected_endnote, actual_endnote)
 
     def test_printing_as_ris(self):
-        fixture = os.path.join("fixtures", "ris-1")
+        fixture = os.path.join("fixtures", "1", "ris.txt")
         with open(fixture) as f:
             expected_ris = f.read()
         actual_ris = self.citation.as_ris()

6ddbdd03729561cb1d78af9de5df1dca6e9facfb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-05-03 10:54:41 +0200

    removed python 3.7 for the moment

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',

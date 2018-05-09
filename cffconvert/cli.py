edb2298bbcb05de3e6bbd780614d84bc21d69d7f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-05-09 13:14:16 +0200

    cffconvert cli can now print the cff; cffconvert cli can print its version;

--- cffconvert/cli.py
+++ cffconvert/cli.py
@@ -1,25 +1,34 @@
 import click
 from cffconvert import Citation
+from cffconvert import version as cffconvert_version
 
 
 @click.command(context_settings=dict(max_content_width=120))
-@click.option("--infile", "-if", type=str, default=None, help="Path to the CITATION.cff input file.")
-@click.option("--outfile", "-of", type=str, default=None, help="Path to the output file.")
-@click.option("--outputformat", "-f", type=str, default=None, help="Output format: bibtex|codemeta|endnote|ris|zenodo",
-              required=True)
-@click.option("--url", "-u", type=str, default=None, help="URL of the repo containing the CITATION.cff (currently only "
-                                                          "github.com is supported; may include branch name, commit " +
-                                                          "sha, tag name). For example: \'https://github.com/" +
-                                                          "citation-file-format/cff-converter-python\' or \'https" +
-                                                          "://github.com/citation-file-format/cff-converter-python/" +
-                                                          "tree/master\'")
-@click.option("--validate", "-v", is_flag=True, default=False, help="Validate the CITATION.cff found at the URL or " +
-                                                                    "supplied through '--infile'")
+@click.option("--infile", "-if", type=str, default=None,
+              help="Path to the CITATION.cff input file.")
+@click.option("--outfile", "-of", type=str, default=None,
+              help="Path to the output file.")
+@click.option("--outputformat", "-f", type=str, default=None,
+              help="Output format: bibtex|cff|codemeta|endnote|ris|zenodo")
+@click.option("--url", "-u", type=str, default=None,
+              help="URL of the repo containing the CITATION.cff (currently only github.com is supported; may " +
+                   "include branch name, commit sha, tag name). For example: \'https://github.com/citation-fi" +
+                   "le-format/cff-converter-python\' or \'https://github.com/citation-file-format/cff-convert" +
+                   "er-python/tree/master\'")
+@click.option("--validate", is_flag=True, default=False,
+              help="Validate the CITATION.cff found at the URL or supplied through '--infile'")
 @click.option("--ignore-suspect-keys", "-ig", is_flag=True, default=False,
               help="If True, ignore any keys from CITATION.cff that are likely out of date, such as \'commit\', " +
                    "\'date-released\', \'doi\', and \'version\'.")
-@click.option("--verbose", "-v", is_flag=True, default=False, help="Provide feedback on what was entered.")
-def cli(infile, outfile, outputformat, url, validate, ignore_suspect_keys, verbose):
+@click.option("--verbose", is_flag=True, default=False,
+              help="Provide feedback on what was entered.")
+@click.option("--version", is_flag=True, default=False,
+              help="Print version and exit.")
+def cli(infile, outfile, outputformat, url, validate, ignore_suspect_keys, verbose, version):
+
+    if version:
+        print("{0}".format(cffconvert_version.__version__))
+        return
 
     if verbose is True:
         click.echo("{0} = {1}".format("infile", infile))
@@ -30,7 +39,7 @@ def cli(infile, outfile, outputformat, url, validate, ignore_suspect_keys, verbo
         click.echo("{0} = {1}".format("ignore_suspect_keys", ignore_suspect_keys))
         click.echo("{0} = {1}".format("verbose", verbose))
 
-    acceptable_output_formats = ["bibtex", "codemeta", "endnote", "ris", "zenodo"]
+    acceptable_output_formats = ["bibtex", "cff", "codemeta", "endnote", "ris", "zenodo"]
     if outputformat not in acceptable_output_formats:
         raise ValueError("'--outputformat' should be one of [{0}]".format(", ".join(acceptable_output_formats)))
 
@@ -49,7 +58,7 @@ def cli(infile, outfile, outputformat, url, validate, ignore_suspect_keys, verbo
         with open(infile, "r") as f:
             cffstr = f.read()
 
-    # TODO currently there is no way to provide values for these 3 arguments
+    # TODO currently there is no way to provide values for these 3 arguments from the command line
     remove = None
     override = None
     suspect_keys = None
@@ -66,6 +75,8 @@ def cli(infile, outfile, outputformat, url, validate, ignore_suspect_keys, verbo
         outstr = citation.as_ris()
     elif outputformat == "zenodo":
         outstr = citation.as_zenodojson()
+    elif outputformat == "cff":
+        outstr = citation.cffstr
 
     if outfile is None:
         print(outstr)

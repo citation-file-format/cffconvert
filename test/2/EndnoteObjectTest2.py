8adc6bbf9a5167b2cf003050aed3421091aed18c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-12-11 17:53:07 +0100

    added EndnoteObject tests for fixtures 2-16

---
+++ test/2/EndnoteObjectTest2.py
@@ -0,0 +1,50 @@
+from cffconvert.EndnoteObject import EndnoteObject
+import unittest
+import os
+import ruamel.yaml as yaml
+
+
+class EndnoteObjectTest2(unittest.TestCase):
+
+    def setUp(self):
+        fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
+        with open(fixture, "r") as f:
+            cffstr = f.read()
+            cff_object = yaml.safe_load(cffstr)
+            self.eo = EndnoteObject(cff_object, initialize_empty=True)
+
+    def test_check_cff_object(self):
+        self.eo.check_cff_object()
+        # doesn't need an assert
+
+    def test_author(self):
+        self.eo.add_author()
+        self.assertEqual(self.eo.author, '%A Fernández de Córdoba Jr., Gonzalo\n')
+
+    def test_doi(self):
+        self.eo.add_doi()
+        self.assertIsNone(self.eo.doi)
+
+    def test_keyword(self):
+        self.eo.add_keyword()
+        self.assertIsNone(self.eo.keyword)
+
+    def test_name(self):
+        self.eo.add_name()
+        self.assertEqual(self.eo.name, '%T example title\n')
+
+    def test_print(self):
+        actual_endnote = self.eo.add_all().print()
+        fixture = os.path.join(os.path.dirname(__file__), "endnote.enw")
+        with open(fixture, "r") as f:
+            expected_endnote = f.read()
+        self.assertEqual(actual_endnote, expected_endnote)
+
+    def test_url(self):
+        self.eo.add_url()
+        self.assertIsNone(self.eo.url)
+
+    def test_year(self):
+        self.eo.add_year()
+        self.assertIsNone(self.eo.year)
+
---
+++ test/3/EndnoteObjectTest3.py
@@ -0,0 +1,24 @@
+from cffconvert.EndnoteObject import EndnoteObject
+import unittest
+import os
+import ruamel.yaml as yaml
+
+
+class EndnoteObjectTest3(unittest.TestCase):
+
+    def setUp(self):
+        fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
+        with open(fixture, "r") as f:
+            cffstr = f.read()
+            cff_object = yaml.safe_load(cffstr)
+            self.eo = EndnoteObject(cff_object, initialize_empty=True)
+
+    def test_check_cff_object(self):
+        self.eo.check_cff_object()
+        # doesn't need an assert
+
+    def test_check_cff_object(self):
+        with self.assertRaises(ValueError) as context:
+            self.eo.check_cff_object()
+        self.assertTrue(str(context.exception).startswith('\'cff-version\':') and
+                        str(context.exception).endswith('isn\'t a supported version.'))

41eab114d46c117d9638673309ed369cefed7d8e
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-24 13:11:20 +0100

    Added class description

--- cffconvert/ApalikeObject.py
+++ cffconvert/ApalikeObject.py
@@ -1,3 +1,16 @@
+# This style loosely tries to implement the guidelines set in the
+# "How to Cite Software in APA Style"[1] blog post. The general idea of an
+# APA-like format is not to rigidly follow APA rules[2], but mostly to provide an
+# output that resembles something that one would find in a scientific
+# publication, particularly following the "usual who-when-what-where format".
+#
+# Example references [1]:
+# Esolang, A. N. (2014). Obscure Reference Generator [Computer software]. Washington, DC: E & K Press.
+# Customized Synergy [Computer software]. (2014). Retrieved from http://customizedsynergy.com
+#
+# [1] https://blog.apastyle.org/apastyle/2015/01/how-to-cite-software-in-apa-style.html
+# [2] https://apastyle.apa.org
+
 class ApalikeObject:
 
     supported_cff_versions = ['1.0.3', '1.1.0']

e6dd203ec6d4ab04cd0959e836699ae23e3e8d19
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-06-24 13:13:02 +0200

    Fixed 1.0.3 contract for 01

--- test/1.0.3/01/ApalikeObjectTest01.py
+++ test/1.0.3/01/ApalikeObjectTest01.py
@@ -21,18 +21,22 @@ class ApalikeObjectTest(Contract, unittest.TestCase):
         self.ao.check_cff_object()
         # doesn't need an assert
 
-    def test_doi(self):
-        self.ao.add_doi()
-        self.assertEqual(self.ao.doi, 'DOI: http://doi.org/10.5281/zenodo.1162057 ')
+    def test_year(self):
+        self.ao.add_year()
+        self.assertEqual(self.ao.year, ' (2018). ')
 
     def test_title(self):
         self.ao.add_title()
         self.assertEqual(self.ao.title, 'cff-converter-python')
 
+    def test_version(self):
+        self.ao.add_version()
+        self.assertEqual(self.ao.version, ' (version 1.0.0). ')
+
+    def test_doi(self):
+        self.ao.add_doi()
+        self.assertEqual(self.ao.doi, 'DOI: http://doi.org/10.5281/zenodo.1162057 ')
+
     def test_url(self):
         self.ao.add_url()
-        self.assertEqual(self.ao.url, 'URL: https://github.com/citation-file-format/cff-converter-python')
-
-    def test_year(self):
-        self.ao.add_year()
-        self.assertEqual(self.ao.year, ' (2018). ')
+        self.assertEqual(self.ao.url, 'URL: https://github.com/citation-file-format/cff-converter-python\n')

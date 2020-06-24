f957f0edcbefc2bfca2f365eb9c9df6c525620d3
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-06-24 07:52:30 +0200

    Added (failing) 1.0.3 test for Apalike

---
+++ test/1.0.3/01/ApalikeObjectTest01.py
@@ -0,0 +1,38 @@
+from cffconvert import ApalikeObject
+import unittest
+import os
+import ruamel.yaml as yaml
+from test.contracts.ApalikeObject import Contract
+
+class ApalikeObjectTest(Contract, unittest.TestCase):
+
+    def setUp(self):
+        fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
+        with open(fixture, "r") as f:
+            cffstr = f.read()
+            cff_object = yaml.safe_load(cffstr)
+            self.ao = ApalikeObject(cff_object, initialize_empty=True)
+
+    def test_author(self):
+        self.ao.add_author()
+        self.assertEqual(self.ao.author, 'Spaaks J.H., Klaver T.')
+
+    def test_check_cff_object(self):
+        self.ao.check_cff_object()
+        # doesn't need an assert
+
+    def test_doi(self):
+        self.ao.add_doi()
+        self.assertEqual(self.ao.doi, 'DOI: http://doi.org/10.5281/zenodo.1162057 ')
+
+    def test_title(self):
+        self.ao.add_title()
+        self.assertEqual(self.ao.title, 'cff-converter-python')
+
+    def test_url(self):
+        self.ao.add_url()
+        self.assertEqual(self.ao.url, 'URL: https://github.com/citation-file-format/cff-converter-python')
+
+    def test_year(self):
+        self.ao.add_year()
+        self.assertEqual(self.ao.year, ' (2018). ')

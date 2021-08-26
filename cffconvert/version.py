ce90b713a17202dda7d7446f62bc7c53c4bcc1ae
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-26 13:13:32 +0200

    updated version str in cffconvert/version.py

--- cffconvert/version.py
+++ cffconvert/version.py
@@ -1,4 +1,4 @@
 # from https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined
 # -in-setup-py-setuptools-in-my-package#answer-24517154
 
-__version__ = "1.4.0-alpha0"
+__version__ = "2.0.0-alpha.0"

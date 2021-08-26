2dd3419836d96fd92615f84e443b96aecce99f43
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-26 18:50:59 +0200

    copied 1.1.0/ tests to 1.2.0/ and updated cff-version string

---
+++ test/1.2.0/01/CliTest01.py
@@ -0,0 +1,230 @@
+import unittest
+import os
+from click.testing import CliRunner
+from cffconvert import cli as cffconvert_cli
+import ruamel.yaml as yaml
+
+
+class CliTests(unittest.TestCase):
+
+    @staticmethod
+    def read_sibling_file(filename):
+        f = os.path.join(os.path.dirname(__file__), filename)
+        with open(f, "r") as f:
+            return f.read()
+
+    def test_local_cff_file_does_not_exist(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cffconvert_cli, ["-f", "bibtex"])
+        self.assertTrue(result.exit_code == -1)
+        self.assertTrue(result.exception.strerror.startswith("No such file or directory"))
+
+    def test_printing_of_help(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cffconvert_cli, ["--help"])
+        self.assertTrue(result.exit_code == 0)
+        self.assertTrue(result.output[:6] == "Usage:")
+
+    def test_printing_of_version(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cffconvert_cli, ["--version"])
+        self.assertTrue(result.exit_code == 0)
+        self.assertEqual(result.output, "1.4.0-alpha0\n")
+
+    def test_printing_on_stdout_as_bibtex(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("bibtex.bib")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "bibtex"])
+        self.assertEqual(result.exit_code, 0)
+        actual = result.output
+        self.assertEqual(expected, actual)
+
+    def test_printing_on_stdout_as_cff(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = yaml.safe_load(cffstr)
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "cff"])
+        self.assertEqual(result.exit_code, 0)
+        actual = yaml.safe_load(result.output)
+        self.assertDictEqual(expected, actual)
+
+    def test_printing_on_stdout_as_codemeta(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("codemeta.json")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "codemeta"])
+        self.assertEqual(result.exit_code, 0)
+        actual = result.output
+        self.assertEqual(expected, actual)
+
+    def test_printing_on_stdout_as_endnote(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("endnote.enw")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "endnote"])
+        self.assertEqual(result.exit_code, 0)
+        actual = result.output
+        self.assertEqual(expected, actual)
+
+    def test_printing_on_stdout_as_ris(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("ris.txt")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "ris"])
+        self.assertEqual(result.exit_code, 0)
+        actual = result.output
+        self.assertEqual(expected, actual)
+
+    def test_printing_on_stdout_as_schemaorg(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("schemaorg.json")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "schema.org"])
+        self.assertEqual(result.exit_code, 0)
+        actual = result.output
+        self.assertEqual(expected, actual)
+
+    def test_printing_when_verbose(self):
+        expected_output = ("{0} = {1}\n" +
+                           "{2} = {3}\n" +
+                           "{4} = {5}\n" +
+                           "{6} = {7}\n" +
+                           "{8} = {9}\n" +
+                           "{10} = {11}\n" +
+                           "{12} = {13}\n" +
+                           "{14} = {15}\n").format("infile", None,
+                                                   "outfile", None,
+                                                   "outputformat", None,
+                                                   "url", None,
+                                                   "validate", False,
+                                                   "ignore_suspect_keys", False,
+                                                   "verbose", True,
+                                                   "version", False)
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cffconvert_cli, ["--verbose"])
+        self.assertTrue(result.exit_code == -1)
+        self.assertEqual(result.output, expected_output)
+
+    def test_raising_valueerror_on_unsupported_format(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "unsupported_97491"])
+        self.assertEqual(result.exit_code, -1)
+        self.assertTrue(isinstance(result.exception, ValueError))
+        self.assertTrue(str(result.exception).startswith('\'--outputformat\' should be one of ['))
+
+    def test_without_arguments(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(cffconvert_cli, [])
+        self.assertTrue(result.exit_code == -1)
+        self.assertTrue(isinstance(result.exception, FileNotFoundError))
+        self.assertEqual(result.exception.strerror, 'No such file or directory')
+
+    def test_writing_as_bibtex(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("bibtex.bib")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "bibtex",
+                                                         "--outfile", "bibtex.bib"])
+            with open("bibtex.bib", "r") as f:
+                actual = f.read()
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(expected, actual)
+
+    def test_writing_as_codemeta(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("codemeta.json")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "codemeta",
+                                                         "--outfile", "codemeta.json"])
+            with open("codemeta.json", "r") as f:
+                actual = f.read()
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(expected, actual)
+
+    def test_writing_as_endnote(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("endnote.enw")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "endnote",
+                                                         "--outfile", "endnote.enw"])
+            with open("endnote.enw", "r") as f:
+                actual = f.read()
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(expected, actual)
+
+    def test_writing_as_ris(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("ris.txt")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "ris",
+                                                         "--outfile", "ris.txt"])
+            with open("ris.txt", "r") as f:
+                actual = f.read()
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(expected, actual)
+
+    def test_writing_as_schemaorg(self):
+        cffstr = CliTests.read_sibling_file("CITATION.cff")
+        expected = CliTests.read_sibling_file("schemaorg.json")
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open("CITATION.cff", "w") as f:
+                f.write(cffstr)
+            result = runner.invoke(cffconvert_cli, ["--outputformat", "schema.org",
+                                                         "--outfile", "schemaorg.json"])
+            with open("schemaorg.json", "r") as f:
+                actual = f.read()
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
+
+
---
+++ test/1.2.0/04/ZenodoObjectTest04.py
@@ -0,0 +1,64 @@
+from cffconvert import ZenodoObject
+import unittest
+import os
+import ruamel.yaml as yaml
+from test.contracts.ZenodoObject import Contract
+
+
+class ZenodoObjectTest(Contract, unittest.TestCase):
+
+    def setUp(self):
+        fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
+        with open(fixture, "r") as f:
+            cffstr = f.read()
+            cff_object = yaml.safe_load(cffstr)
+            self.zo = ZenodoObject(cff_object, initialize_empty=True)
+
+    def test_check_cff_object(self):
+        self.zo.check_cff_object()
+        # doesn't need an assert
+
+    def test_creators(self):
+        self.zo.add_creators()
+        expected_creators = [
+            {
+                "affiliation": "Netherlands eScience Center",
+                "name": "Spaaks, Jurriaan H."
+            },
+            {
+                "affiliation": "Netherlands eScience Center",
+                "name": "Klaver, Tom"
+            }
+        ]
+        self.assertListEqual(self.zo.creators, expected_creators)
+
+    def test_doi(self):
+        self.zo.add_doi()
+        self.assertEqual(self.zo.doi, '10.5281/zenodo.1162057')
+
+    def test_keywords(self):
+        self.zo.add_keywords()
+        self.assertEqual(self.zo.keywords, ['citation', 'bibliography', 'cff', 'CITATION.cff'])
+
+    def test_license(self):
+        self.zo.add_license()
+        self.assertEqual(self.zo.license, dict(id='Apache-2.0'))
+
+    def test_print(self):
+        actual_zenodo = self.zo.add_all().print()
+        fixture = os.path.join(os.path.dirname(__file__), ".zenodo.json")
+        with open(fixture, "r") as f:
+            expected_zenodo = f.read()
+        self.assertEqual(actual_zenodo, expected_zenodo)
+
+    def test_publication_date(self):
+        self.zo.add_publication_date()
+        self.assertEqual(self.zo.publication_date, '2018-01-16')
+
+    def test_title(self):
+        self.zo.add_title()
+        self.assertEqual(self.zo.title, 'cff-converter-python')
+
+    def test_version(self):
+        self.zo.add_version()
+        self.assertEqual(self.zo.version, '1.0.0')

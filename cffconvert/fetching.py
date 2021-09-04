4f07ce93499b2668f4b3ab3e5d0862506ed6a7a1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-09-04 08:36:40 +0200

    added placeholder cff string for fetching from url

--- cffconvert/fetching.py
+++ cffconvert/fetching.py
@@ -1,2 +1,2 @@
 def read_from_url(url):
-    return "title: thetitle"
+    return "title: thetitle\nauthors:\n  - name: theauthor\ncff-version: 1.0.2\nmessage: hello message\ndate-released: 1900-12-31\nversion: \"1\"\ndoi: 10.0000/FIXME\nreferences:\n  - type: generic\n    title: reftitle\n    authors:\n      - name: refauthors\n"

c139850062c89e8881a83a0e69cc737bb595c223
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-09-06 11:12:47 +0200

    re-added simple fetching; facilitates simple rawification of certain links; refs #180

--- cffconvert/fetching.py
+++ cffconvert/fetching.py
@@ -1,2 +1,22 @@
+import requests
+
+
 def read_from_url(url):
-    return "title: thetitle\nauthors:\n  - name: theauthor\ncff-version: 1.0.2\nmessage: hello message\ndate-released: 1900-12-31\nversion: \"1\"\ndoi: 10.0000/FIXME\nreferences:\n  - type: generic\n    title: reftitle\n    authors:\n      - name: refauthors\n"
+    if not url.startswith("https://"):
+        raise AssertionError("URL should be an https:// link")
+
+    url_fetch = rawify_github_link(url)
+
+    r = requests.get(url_fetch)
+    if r.ok:
+        return r.text
+    else:
+        raise Exception("Error while trying to retrieve {0}".format(url_fetch))
+
+
+def rawify_github_link(url):
+    # just a placeholder replacement for the moment
+    if url == "https://github.com/citation-file-format/cff-converter-python":
+        return "https://raw.githubusercontent.com/citation-file-format/cff-converter-python/main/CITATION.cff"
+    # return unrecognized URLs as-is
+    return url

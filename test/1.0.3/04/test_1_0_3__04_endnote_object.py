a0c447a20f438c84d8094e045a1b9b692edffda1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-09-14 13:01:21 +0200

    converted endnote tests to oneliners; refs #221

--- test/1.0.3/04/test_1_0_3__04_endnote_object.py
+++ test/1.0.3/04/test_1_0_3__04_endnote_object.py
@@ -24,8 +24,7 @@ class TestEndnoteObject(Contract):
         assert actual_endnote == expected_endnote
 
     def test_author(self, endnote_object):
-        endnote_object.add_author()
-        assert endnote_object.author == '%A Spaaks, Jurriaan H.\n%A Klaver, Tom\n' + \
+        assert endnote_object.add_author().author == '%A Spaaks, Jurriaan H.\n%A Klaver, Tom\n' + \
                                         '%A Verhoeven, Stefan\n%A Druskat, Stephan\n'
 
     def test_check_cffobj(self, endnote_object):
@@ -33,21 +32,16 @@ class TestEndnoteObject(Contract):
         # doesn't need an assert
 
     def test_doi(self, endnote_object):
-        endnote_object.add_doi()
-        assert endnote_object.doi == '%R 10.5281/zenodo.1162057\n'
+        assert endnote_object.add_doi().doi == '%R 10.5281/zenodo.1162057\n'
 
     def test_keyword(self, endnote_object):
-        endnote_object.add_keyword()
-        assert endnote_object.keyword == '%K citation\n%K bibliography\n%K cff\n%K CITATION.cff\n'
+        assert endnote_object.add_keyword().keyword == '%K citation\n%K bibliography\n%K cff\n%K CITATION.cff\n'
 
     def test_name(self, endnote_object):
-        endnote_object.add_name()
-        assert endnote_object.name == '%T cffconvert\n'
+        assert endnote_object.add_name().name == '%T cffconvert\n'
 
     def test_url(self, endnote_object):
-        endnote_object.add_url()
-        assert endnote_object.url == '%U https://github.com/citation-file-format/cff-converter-python\n'
+        assert endnote_object.add_url().url == '%U https://github.com/citation-file-format/cff-converter-python\n'
 
     def test_year(self, endnote_object):
-        endnote_object.add_year()
-        assert endnote_object.year == '%D 2018\n'
+        assert endnote_object.add_year().year == '%D 2018\n'
--- test/1.1.0/07/test_1_1_0__07_endnote_object.py
+++ test/1.1.0/07/test_1_1_0__07_endnote_object.py
@@ -24,29 +24,23 @@ class TestEndnoteObject(Contract):
         assert actual_endnote == expected_endnote
 
     def test_author(self, endnote_object):
-        endnote_object.add_author()
-        assert endnote_object.author == '%A Spaaks, Jurriaan H.\n%A Klaver, Tom\n%A mysteryauthor\n'
+        assert endnote_object.add_author().author == '%A Spaaks, Jurriaan H.\n%A Klaver, Tom\n%A mysteryauthor\n'
 
     def test_check_cffobj(self, endnote_object):
         endnote_object.check_cffobj()
         # doesn't need an assert
 
     def test_doi(self, endnote_object):
-        endnote_object.add_doi()
-        assert endnote_object.doi == '%R 10.5281/zenodo.1162057\n'
+        assert endnote_object.add_doi().doi == '%R 10.5281/zenodo.1162057\n'
 
     def test_keyword(self, endnote_object):
-        endnote_object.add_keyword()
-        assert endnote_object.keyword == '%K citation\n%K bibliography\n%K cff\n%K CITATION.cff\n'
+        assert endnote_object.add_keyword().keyword == '%K citation\n%K bibliography\n%K cff\n%K CITATION.cff\n'
 
     def test_name(self, endnote_object):
-        endnote_object.add_name()
-        assert endnote_object.name == '%T cff-converter-python\n'
+        assert endnote_object.add_name().name == '%T cff-converter-python\n'
 
     def test_url(self, endnote_object):
-        endnote_object.add_url()
-        assert endnote_object.url == '%U https://github.com/citation-file-format/cff-converter-python\n'
+        assert endnote_object.add_url().url == '%U https://github.com/citation-file-format/cff-converter-python\n'
 
     def test_year(self, endnote_object):
-        endnote_object.add_year()
-        assert endnote_object.year == '%D 2018\n'
+        assert endnote_object.add_year().year == '%D 2018\n'

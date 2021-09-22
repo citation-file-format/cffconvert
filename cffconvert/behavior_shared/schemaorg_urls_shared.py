b060ea0d55327e09848489053de208484a21f9db
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-09-22 14:40:36 +0200

    deduplicated some url shared methods

--- cffconvert/behavior_shared/schemaorg_urls_shared.py
+++ cffconvert/behavior_shared/schemaorg_urls_shared.py
@@ -1,11 +1,12 @@
 from abc import abstractmethod
+from cffconvert.behavior_shared.abstract_url_shared import AbstractUrlShared
 
 
 # pylint: disable=too-few-public-methods
-class SchemaorgUrlsShared:
+class SchemaorgUrlsShared(AbstractUrlShared):
 
     def __init__(self, cffobj):
-        self._cffobj = cffobj
+        super().__init__(cffobj)
         self._behaviors = {
             'IRACU': self._from_identifiers_url_and_repository_code,
             'IRAC_': self._from_identifiers_url_and_repository_code,
@@ -83,40 +84,6 @@ class SchemaorgUrlsShared:
 
     def _from_url(self):
         return None, self._cffobj.get('url')
-
-    def _get_urls_from_identifiers(self):
-        identifiers = self._cffobj.get('identifiers', [])
-        return [identifier for identifier in identifiers if identifier.get('type') == 'url']
-
-    def _has_identifiers_url(self):
-        urls = self._get_urls_from_identifiers()
-        if len(urls) > 0:
-            return 'I'
-        return '_'
-
-    def _has_repository(self):
-        tmp = self._cffobj.get('repository', None)
-        if tmp is not None and tmp != '':
-            return 'R'
-        return '_'
-
-    def _has_repository_artifact(self):
-        tmp = self._cffobj.get('repository-artifact', None)
-        if tmp is not None and tmp != '':
-            return 'A'
-        return '_'
-
-    def _has_repository_code(self):
-        tmp = self._cffobj.get('repository-code', None)
-        if tmp is not None and tmp != '':
-            return 'C'
-        return '_'
-
-    def _has_url(self):
-        tmp = self._cffobj.get('url', None)
-        if tmp is not None and tmp != '':
-            return 'U'
-        return '_'
 
     @abstractmethod
     def as_tuple(self):

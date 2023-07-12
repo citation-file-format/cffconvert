0cfa3dc93f293ac3c9eeddecaa0c79300102ca74
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-12 13:42:34 +0200

    simplified testing pattern for codemeta_object

--- test/1.0.3/03/test_1_0_3__03_codemeta_object.py
+++ test/1.0.3/03/test_1_0_3__03_codemeta_object.py
@@ -1,11 +1,9 @@
 import os
 from test.contracts.codemeta_object import Contract
-import pytest
 from cffconvert import Citation
 from cffconvert.behavior_1_0_x.codemeta_object import CodemetaObject
 
 
-@pytest.fixture(scope="module")
 def codemeta_object():
     fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
     with open(fixture, "rt", encoding="utf-8") as f:
@@ -16,15 +14,15 @@ def codemeta_object():
 
 class TestCodemetaObject(Contract):
 
-    def test_as_string(self, codemeta_object):
-        actual_codemeta = codemeta_object.add_all().as_string()
+    def test_as_string(self):
+        actual_codemeta = codemeta_object().add_all().as_string()
         fixture = os.path.join(os.path.dirname(__file__), "codemeta.json")
         with open(fixture, "rt", encoding="utf-8") as f:
             expected_codemeta = f.read()
         assert actual_codemeta == expected_codemeta
 
-    def test_author(self, codemeta_object):
-        assert codemeta_object.add_author().author == [{
+    def test_author(self):
+        assert codemeta_object().add_author().author == [{
             "@type": "Person",
             "givenName": "Jisk",
             "familyName": "Attema",
@@ -43,38 +41,38 @@ class TestCodemetaObject(Contract):
             }
         }]
 
-    def test_check_cffobj(self, codemeta_object):
-        codemeta_object.check_cffobj()
+    def test_check_cffobj(self):
+        codemeta_object().check_cffobj()
         # doesn't need an assert
 
-    def test_code_repository(self, codemeta_object):
-        assert codemeta_object.add_urls().code_repository == 'https://github.com/NLeSC/spot'
+    def test_code_repository(self):
+        assert codemeta_object().add_urls().code_repository == 'https://github.com/NLeSC/spot'
 
-    def test_date_published(self, codemeta_object):
-        assert codemeta_object.add_date_published().date_published == '2017-10-07'
+    def test_date_published(self):
+        assert codemeta_object().add_date_published().date_published == '2017-10-07'
 
-    def test_description(self, codemeta_object):
-        assert codemeta_object.add_description().description is None
+    def test_description(self):
+        assert codemeta_object().add_description().description is None
 
-    def test_identifier(self, codemeta_object):
-        assert codemeta_object.add_identifier().identifier == 'https://doi.org/10.5281/zenodo.1003346'
+    def test_identifier(self):
+        assert codemeta_object().add_identifier().identifier == 'https://doi.org/10.5281/zenodo.1003346'
 
-    def test_keywords(self, codemeta_object):
-        assert codemeta_object.add_keywords().keywords == [
+    def test_keywords(self):
+        assert codemeta_object().add_keywords().keywords == [
             'visualization',
             'big data',
             'visual data analytics',
             'multi-dimensional data'
         ]
 
-    def test_license(self, codemeta_object):
-        assert codemeta_object.add_license().license == 'https://spdx.org/licenses/Apache-2.0'
+    def test_license(self):
+        assert codemeta_object().add_license().license == 'https://spdx.org/licenses/Apache-2.0'
 
-    def test_name(self, codemeta_object):
-        assert codemeta_object.add_name().name == 'spot'
+    def test_name(self):
+        assert codemeta_object().add_name().name == 'spot'
 
-    def test_url(self, codemeta_object):
-        assert codemeta_object.add_urls().url == 'https://github.com/NLeSC/spot'
+    def test_url(self):
+        assert codemeta_object().add_urls().url == 'https://github.com/NLeSC/spot'
 
-    def test_version(self, codemeta_object):
-        assert codemeta_object.add_version().version == '0.1.0'
+    def test_version(self):
+        assert codemeta_object().add_version().version == '0.1.0'
--- test/1.1.0/08/test_1_1_0__08_codemeta_object.py
+++ test/1.1.0/08/test_1_1_0__08_codemeta_object.py
@@ -1,11 +1,9 @@
 import os
 from test.contracts.codemeta_object import Contract
-import pytest
 from cffconvert import Citation
 from cffconvert.behavior_1_1_x.codemeta_object import CodemetaObject
 
 
-@pytest.fixture(scope="module")
 def codemeta_object():
     fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
     with open(fixture, "rt", encoding="utf-8") as f:
@@ -16,15 +14,15 @@ def codemeta_object():
 
 class TestCodemetaObject(Contract):
 
-    def test_as_string(self, codemeta_object):
-        actual_codemeta = codemeta_object.add_all().as_string()
+    def test_as_string(self):
+        actual_codemeta = codemeta_object().add_all().as_string()
         fixture = os.path.join(os.path.dirname(__file__), "codemeta.json")
         with open(fixture, "rt", encoding="utf-8") as f:
             expected_codemeta = f.read()
         assert actual_codemeta == expected_codemeta
 
-    def test_author(self, codemeta_object):
-        assert codemeta_object.add_author().author == [{
+    def test_author(self):
+        assert codemeta_object().add_author().author == [{
             "@type": "Person",
             "affiliation": {
                 "@type": "Organization",
@@ -42,33 +40,33 @@ class TestCodemetaObject(Contract):
             "givenName": "Steven"
         }]
 
-    def test_check_cffobj(self, codemeta_object):
-        codemeta_object.check_cffobj()
+    def test_check_cffobj(self):
+        codemeta_object().check_cffobj()
         # doesn't need an assert
 
-    def test_code_repository(self, codemeta_object):
-        assert codemeta_object.add_urls().code_repository is None
+    def test_code_repository(self):
+        assert codemeta_object().add_urls().code_repository is None
 
-    def test_date_published(self, codemeta_object):
-        assert codemeta_object.add_date_published().date_published == '2018-01-16'
+    def test_date_published(self):
+        assert codemeta_object().add_date_published().date_published == '2018-01-16'
 
-    def test_description(self, codemeta_object):
-        assert codemeta_object.add_description().description is None
+    def test_description(self):
+        assert codemeta_object().add_description().description is None
 
-    def test_identifier(self, codemeta_object):
-        assert codemeta_object.add_identifier().identifier is None
+    def test_identifier(self):
+        assert codemeta_object().add_identifier().identifier is None
 
-    def test_keywords(self, codemeta_object):
-        assert codemeta_object.add_keywords().keywords is None
+    def test_keywords(self):
+        assert codemeta_object().add_keywords().keywords is None
 
-    def test_license(self, codemeta_object):
-        assert codemeta_object.add_license().license is None
+    def test_license(self):
+        assert codemeta_object().add_license().license is None
 
-    def test_name(self, codemeta_object):
-        assert codemeta_object.add_name().name == 'cff-converter-python'
+    def test_name(self):
+        assert codemeta_object().add_name().name == 'cff-converter-python'
 
-    def test_url(self, codemeta_object):
-        assert codemeta_object.add_urls().url is None
+    def test_url(self):
+        assert codemeta_object().add_urls().url is None
 
-    def test_version(self, codemeta_object):
-        assert codemeta_object.add_version().version == '1.0.0'
+    def test_version(self):
+        assert codemeta_object().add_version().version == '1.0.0'

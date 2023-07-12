fc9088cea39ececbbedca2b605ee0e1ee7b55fff
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-12 13:26:56 +0200

    simplified testing pattern for bibtex_object

--- test/1.2.0/urls/I__C_/test_1_2_0_urls_I__C__bibtex_object.py
+++ test/1.2.0/urls/I__C_/test_1_2_0_urls_I__C__bibtex_object.py
@@ -1,11 +1,9 @@
 import os
 from test.contracts.bibtex_object import Contract
-import pytest
 from cffconvert import Citation
 from cffconvert.behavior_1_2_x.bibtex_object import BibtexObject
 
 
-@pytest.fixture(scope="module")
 def bibtex_object():
     fixture = os.path.join(os.path.dirname(__file__), "CITATION.cff")
     with open(fixture, "rt", encoding="utf-8") as f:
@@ -16,31 +14,31 @@ def bibtex_object():
 
 class TestBibtexObject(Contract):
 
-    def test_author(self, bibtex_object):
-        assert bibtex_object.add_author().author == 'author = {Test author}'
+    def test_author(self):
+        assert bibtex_object().add_author().author == 'author = {Test author}'
 
-    def test_check_cffobj(self, bibtex_object):
-        bibtex_object.check_cffobj()
+    def test_check_cffobj(self):
+        bibtex_object().check_cffobj()
         # doesn't need an assert
 
-    def test_doi(self, bibtex_object):
-        assert bibtex_object.add_doi().doi is None
+    def test_doi(self):
+        assert bibtex_object().add_doi().doi is None
 
-    def test_month(self, bibtex_object):
-        assert bibtex_object.add_month().month is None
+    def test_month(self):
+        assert bibtex_object().add_month().month is None
 
-    def test_as_string(self, bibtex_object):
-        actual_bibtex = bibtex_object.add_all().as_string()
+    def test_as_string(self):
+        actual_bibtex = bibtex_object().add_all().as_string()
         fixture = os.path.join(os.path.dirname(__file__), "bibtex.bib")
         with open(fixture, "rt", encoding="utf-8") as f:
             expected_bibtex = f.read()
         assert actual_bibtex == expected_bibtex
 
-    def test_title(self, bibtex_object):
-        assert bibtex_object.add_title().title == 'title = {Test title}'
+    def test_title(self):
+        assert bibtex_object().add_title().title == 'title = {Test title}'
 
-    def test_url(self, bibtex_object):
-        assert bibtex_object.add_url().url == 'url = {https://github.com/the-url-from-identifiers}'
+    def test_url(self):
+        assert bibtex_object().add_url().url == 'url = {https://github.com/the-url-from-identifiers}'
 
-    def test_year(self, bibtex_object):
-        assert bibtex_object.add_year().year is None
+    def test_year(self):
+        assert bibtex_object().add_year().year is None

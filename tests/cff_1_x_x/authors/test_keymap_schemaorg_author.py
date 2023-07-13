725595d49d5b2ed0a55835426351b0e34d352afd
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-13 20:07:15 +0200

    added test to keymaps to verify there are no unused implementations

--- tests/cff_1_x_x/authors/test_keymap_schemaorg_author.py
+++ tests/cff_1_x_x/authors/test_keymap_schemaorg_author.py
@@ -1,6 +1,7 @@
 # pylint:disable = protected-access
 import types
 import pytest
+import inspect
 from cffconvert.cff_1_x_x.schemaorg_author import SchemaorgAuthor
 from .get_every_key import get_every_key
 
@@ -16,3 +17,11 @@ def test_number_of_keys():
     expected = len(get_every_key())
     actual = len(SchemaorgAuthor(author=None)._behaviors)
     assert actual == expected
+
+
+def test_all_methods_used():
+    author = SchemaorgAuthor(author=None)
+    used_method_names = [elem.__name__ for elem in author._behaviors.values()]
+    implementation_names = [k for k, v in inspect.getmembers(author, predicate=inspect.ismethod) if k.startswith("_from")]
+    for implementation_name in implementation_names:
+        assert implementation_name in used_method_names
--- tests/cff_1_x_x/urls/test_keymap_apalike_url.py
+++ tests/cff_1_x_x/urls/test_keymap_apalike_url.py
@@ -1,6 +1,7 @@
 # pylint:disable = protected-access
 import types
 import pytest
+import inspect
 from cffconvert.cff_1_x_x.apalike_url import ApalikeUrl
 from .get_every_key import get_every_key
 
@@ -16,3 +17,11 @@ def test_number_of_keys():
     expected = len(get_every_key())
     actual = len(ApalikeUrl({})._behaviors)
     assert actual == expected
+
+
+def test_all_methods_used():
+    author = ApalikeUrl({})
+    used_method_names = [elem.__name__ for elem in author._behaviors.values()]
+    implementation_names = [k for k, v in inspect.getmembers(author, predicate=inspect.ismethod) if k.startswith("_from")]
+    for implementation_name in implementation_names:
+        assert implementation_name in used_method_names

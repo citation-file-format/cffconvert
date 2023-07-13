82da6e8e4ec194fa336361bc903a0200a1f6ff82
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-13 20:13:06 +0200

    fixed linter errors

--- tests/cff_1_x_x/authors/test_keymap_ris_author.py
+++ tests/cff_1_x_x/authors/test_keymap_ris_author.py
@@ -1,7 +1,7 @@
 # pylint:disable = protected-access
+import inspect
 import types
 import pytest
-import inspect
 from cffconvert.cff_1_x_x.ris_author import RisAuthor
 from .get_every_key import get_every_key
 
@@ -22,6 +22,8 @@ def test_number_of_keys():
 def test_all_methods_used():
     author = RisAuthor(author=None)
     used_method_names = [elem.__name__ for elem in author._behaviors.values()]
-    implementation_names = [k for k, v in inspect.getmembers(author, predicate=inspect.ismethod) if k.startswith("_from")]
+    implementation_names = [
+        k for k, v in inspect.getmembers(author, predicate=inspect.ismethod) if k.startswith("_from")
+    ]
     for implementation_name in implementation_names:
         assert implementation_name in used_method_names

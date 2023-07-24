84272961d569436b4a5721319918c30799541333
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-24 16:06:29 +0200

    moved tests/contracts to tests/lib/contracts and updated imports; refs #276

--- tests/lib/cff_1_1_0/d/test_endnote_object.py
+++ tests/lib/cff_1_1_0/d/test_endnote_object.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from cffconvert import Citation
 from cffconvert.lib.cff_1_1_x.endnote import EndnoteObject
-from tests.contracts.endnote_object import Contract
+from tests.lib.contracts.endnote_object import Contract
 
 
 def endnote_object():
--- tests/lib/cff_1_2_0/authors/one/GFA_AO_/test_ris_object.py
+++ tests/lib/cff_1_2_0/authors/one/GFA_AO_/test_ris_object.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from cffconvert import Citation
 from cffconvert.lib.cff_1_2_x.ris import RisObject
-from tests.contracts.ris_object import Contract
+from tests.lib.contracts.ris_object import Contract
 
 
 def ris_object():

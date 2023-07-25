09d512ea381bf5e8b920eae7cb841bb70609e1bd
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-07-25 15:38:45 +0200

    updated making calls to github api to retrieve the default branch (#263), to request with mime type, and to have the option of making the call authenticated (#353)

--- src/cffconvert/cli/rawify_url.py
+++ src/cffconvert/cli/rawify_url.py
@@ -1,3 +1,8 @@
+import os
+import requests
+from cffconvert.cli.constants import github_api_version_header
+
+
 def rawify_url(url):
     if url.startswith("https://github.com"):
         urlparts = url.replace("https://github.com", "", 1).strip("/").split("/") + [None] * 5
@@ -6,7 +11,22 @@ def rawify_url(url):
         assert ownername is not None, "URL should include the name of the owner/organization."
         assert reponame is not None, "URL should include the name of the repository."
         if refvalue is None:
-            refvalue = "main"
+            default_branch = None
+            try:
+                repos_api = f"https://api.github.com/repos/{ownername}/{reponame}"
+                headers = github_api_version_header
+                headers.update({"Accept": "application/vnd.github+json"})
+                token = os.environ.get("CFFCONVERT_GITHUB_API_TOKEN")
+                if token is None:
+                    # Proceed with making the call without authenticating -- stricter rate limits apply
+                    pass
+                else:
+                    headers.update({"Authorization": f"Bearer { token }"})
+                response = requests.get(repos_api, headers=headers, timeout=10)
+                if response.ok:
+                    default_branch = response.json().get("default_branch")
+            finally:
+                refvalue = default_branch or "main"
         if filename == "":
             filename = "CITATION.cff"
         return f"https://raw.githubusercontent.com/{ownername}/{reponame}/{refvalue}/{filename}"

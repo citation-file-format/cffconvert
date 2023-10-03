2e1fc5cae98075526f392a897d7f8d7d62de100c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-10-03 15:35:13 +0200

    updated the parameterization of the relation type test such that the terms align with the proposed list of terms in CFF schema; split the list into a list that can be translated to zenodo and one that cannot

--- tests/lib/cff_1_3_0/identifiers/relation/test_zenodo_related_identifiers.py
+++ tests/lib/cff_1_3_0/identifiers/relation/test_zenodo_related_identifiers.py
@@ -14,42 +14,53 @@ def get_cffstr():
 
 def get_relation_types():
     return [
-        "cites",
-        "compiles",
-        "continues",
-        "describes",
-        "documents",
-        "hasPart",
-        "isAlternateIdentifier",
-        "isCitedBy",
-        "isCompiledBy",
-        "isContinuedBy",
-        "isDerivedFrom",
-        "isDescribedBy",
-        "isDocumentedBy",
-        "isIdenticalTo",
-        "isNewVersionOf",
-        "isObsoletedBy",
-        "isPartOf",
-        "isPreviousVersionOf",
-        "isPublishedIn",
-        "isReferencedBy",
-        "isRequiredBy",
-        "isReviewedBy",
-        "isSourceOf",
-        "isSupplementedBy",
-        "isSupplementTo",
-        "obsoletes",
-        "references",
-        "requires",
-        "reviews"
+        "Cites",
+        "Compiles",
+        "Continues",
+        "Describes",
+        "Documents",
+        "HasPart",
+        "IsCitedBy",
+        "IsCompiledBy",
+        "IsContinuedBy",
+        "IsDerivedFrom",
+        "IsDescribedBy",
+        "IsDocumentedBy",
+        "IsIdenticalTo",
+        "IsNewVersionOf",
+        "IsObsoletedBy",
+        "IsPartOf",
+        "IsPreviousVersionOf",
+        "IsPublishedIn",
+        "IsReferencedBy",
+        "IsRequiredBy",
+        "IsReviewedBy",
+        "IsSourceOf",
+        "IsSupplementedBy",
+        "IsSupplementTo",
+        "Obsoletes",
+        "References",
+        "Requires",
+        "Reviews"
+    ]
+
+
+def get_relation_types_skip():
+    return [
+        "HasMetadata",
+        "HasVersion",
+        "IsMetadataFor",
+        "IsOriginalFormOf",
+        "IsVariantFormOf",
+        "IsVersionOf"
     ]
 
 
 @pytest.mark.lib
+@pytest.mark.zenodo
 @pytest.mark.parametrize("expected", get_relation_types())
 def test_with_relation(expected):
-    cffstr = get_cffstr().replace("isCompiledBy", expected)
+    cffstr = get_cffstr().replace("IsCompiledBy", expected)
     citation = Citation(cffstr)
     zenodo_object = ZenodoObject(citation.cffobj, initialize_empty=True)
     related_identifiers = zenodo_object.add_related_identifiers().related_identifiers
@@ -60,12 +71,32 @@ def test_with_relation(expected):
     assert isinstance(related_identifiers[0], dict)
     # pylint: disable=unsubscriptable-object
     actual = related_identifiers[0].get("relation")
-    assert actual == expected
+    assert actual[1:] == expected[1:]
+    assert actual[0] == expected[0].lower()
 
 
 @pytest.mark.lib
+@pytest.mark.zenodo
+@pytest.mark.parametrize("expected", get_relation_types_skip())
+def test_with_relation_unmapped(expected):
+    cffstr = get_cffstr().replace("IsCompiledBy", expected)
+    citation = Citation(cffstr)
+    zenodo_object = ZenodoObject(citation.cffobj, initialize_empty=True)
+    related_identifiers = zenodo_object.add_related_identifiers().related_identifiers
+    assert related_identifiers is not None
+    assert isinstance(related_identifiers, list)
+    assert len(related_identifiers) > 0
+    # pylint: disable=unsubscriptable-object
+    assert isinstance(related_identifiers[0], dict)
+    # pylint: disable=unsubscriptable-object
+    actual = related_identifiers[0].get("relation")
+    assert actual == "isSupplementedBy"
+
+
+@pytest.mark.lib
+@pytest.mark.zenodo
 def test_without_relation():
-    cffstr = get_cffstr().replace("relation: isCompiledBy\n    ", "")
+    cffstr = get_cffstr().replace("relation: IsCompiledBy\n    ", "")
     citation = Citation(cffstr)
     zenodo_object = ZenodoObject(citation.cffobj, initialize_empty=True)
     related_identifiers = zenodo_object.add_related_identifiers().related_identifiers
